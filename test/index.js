aae5a11aaba5c8bcafc03d7f1c2617cf14dc560e
Mohamed Turki <turkipro@gmail.com>
2016-03-15 11:00:18 +0100

    Add Android detection.

--- test/index.js
+++ test/index.js
@@ -16,4 +16,12 @@ describe('browser-info', function() {
 
   });
 
+  it('should detect Android', function(done) {
+     GLOBAL.navigator = {
+         userAgent: 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.23 Mobile Safari/537.36'
+     };
+     should(info().os).equal('Android');
+     done();
+ });
+
 });
